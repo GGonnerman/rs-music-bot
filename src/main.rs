2b9243479044d5aeb4cc8f4df01ca622030d9c45
DeusSeos <45889782+DeusSeos@users.noreply.github.com>
2026-01-21 16:06:23 -0500

    Example code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,45 @@
-#[allow(unused_imports)]
+#![cfg_attr(debug_assertions, allow(dead_code, unused_imports))]
+// std library imports
+use std::env;
+
+// serenity and songbird imports
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+
+// tokio related imports
 use tracing::{debug, error, info, span, warn, Level};
+
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {why:?}");
+            }
+        }
+    }
+}
+
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    // Login with a bot token from the environment
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    // Set gateway intents, which decides what events the bot will be notified about
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
 
+    // Create a new instance of the Client, logging in as a bot.
+    let mut client =
+        Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+
+    // Start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
 }
